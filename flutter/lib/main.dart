af20ab215ce1d10446667c4b4477c885885ccb89
Ashraf <55182461+thrashraf@users.noreply.github.com>
2023-06-03 15:06:23 +0800

    update pod

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -11,7 +11,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
 
   runApp(
     MultiProvider(
